c5236e4daf8ae5baf6f149ea15c98e395438cb7d
carlos <robertorsilva.krlos@gmail.com>
2025-03-18 20:26:04 -0300

    first commit

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,10 @@ import env from './config/env'
 
 const app = setupApp()
 
+app.get('/ola', (req, res)=> {
+  res.json('ola mundo')
+})
+
 app.listen(env.port, async () => {
   console.log(`rodando na porta ${env.port}`)
 })
