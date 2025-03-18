0e4bb6bf9c47893bd63a00c6687bceb15e7a35aa
carlos <robertorsilva.krlos@gmail.com>
2025-03-18 19:22:22 -0300

    first commit

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -1,11 +1,9 @@
 import { type Middleware } from '@/presentation/protocols'
 import { type Response, type NextFunction } from 'express'
-import jwt from 'jsonwebtoken'
 import { type CustomRequest } from './custom-accountId'
 
 export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: CustomRequest, res: Response, next: NextFunction) => {
-    const decodeJwt = jwt.decode(req.headers?.authorization?.replace('Bearer ', '')) as any
     const request = {
       accessToken: req.headers?.authorization?.replace('Bearer ', ''),
       ...(req.headers || {}),
@@ -13,7 +11,6 @@ export const adaptMiddleware = (middleware: Middleware) => {
     }
     const httpResponse = await middleware.handle(request)
     if (httpResponse.statusCode === 200) {
-      Object.assign(req, { ...httpResponse.body, accountId: decodeJwt?.sub as string })
       next()
     } else {
       res.status(httpResponse.statusCode).json({
