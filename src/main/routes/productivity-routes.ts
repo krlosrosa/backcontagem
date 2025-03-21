588fc6bdfaf84ae3e993ea5ecc7c902a58b2aa4e
carlos <robertorsilva.krlos@gmail.com>
2025-03-21 12:46:11 -0300

    first commit

--- src/main/routes/productivity-routes.ts
+++ src/main/routes/productivity-routes.ts
@@ -3,5 +3,8 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddNewCounted } from '../factories/controllers/addNewCounted'
 
 export default (router: Router): void => {
+  router.get('/teste', (req, res)=> {
+    res.json('ok')
+  })
   router.post('/cadastrarcontagem', adaptRoute(makeAddNewCounted()))
 }
